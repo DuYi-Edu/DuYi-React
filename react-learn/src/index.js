53090936b96c1130bf083b7a7abad440441d9041
yuanjin <yuanjinmoke@gmail.com>
2019-07-17 15:49:02 +0800

    2-10旧版生命周期

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
-ReactDOM.render(<h1>Hello World <span>span元素</span></h1>, document.getElementById('root'));
+ReactDOM.render((
+    <App/>
+), document.getElementById('root'));
