c264f891d69e77ad3f6d605cd1f026609b3d8399
yuanjin <yuanjinmoke@gmail.com>
2019-07-18 17:25:47 +0800

    fixed

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,12 +1,7 @@
 import React from 'react'
-// import Test from "./components/common/CheckBoxGroup/Test"
-// import Test from "./components/common/RadioBoxGroup/Test"
-import Test from "./components/common/Select/Test"
-
 export default function App() {
     return (
         <div>
-            <Test/>
         </div>
     )
 }
