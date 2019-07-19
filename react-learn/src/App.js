10bf6d811dab617519606a981675116d1824944f
yuanjin <yuanjinmoke@gmail.com>
2019-07-19 15:36:39 +0800

    fixed

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Test from "./components/common/Select/Test"
 
 export default function App() {
     return (
         <div>
-            <Test/>
         </div>
     )
 }
