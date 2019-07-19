215c3f2cd41bc3d05a25f4766fd11ae8444c9d5c
yuanjin <yuanjinmoke@gmail.com>
2019-07-19 13:00:00 +0800

    fixed

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Test from "./components/common/CheckBoxGroup/Test"
 
 export default function App() {
     return (
         <div>
-            <Test/>
         </div>
     )
 }
