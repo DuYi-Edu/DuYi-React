32e1e4c3738215458f1ce409ab4b09e150310249
yuanjin <yuanjinmoke@gmail.com>
2019-07-23 14:39:58 +0800

    旧版API

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import OldContext from "./OldContext"
-
+import OldContext from "./OldContext";
 export default class App extends React.Component {
     render() {
         return (
-            <OldContext/>
+            <OldContext />
         )
     }
 }
--- react-learn/src/OldContext.js
+++ react-learn/src/OldContext.js
@@ -1,44 +1,77 @@
 import React, { Component } from 'react'
 import PropTypes from "prop-types";
 
+const types = {
+    a: PropTypes.number,
+    b: PropTypes.string.isRequired,
+    onChangeA: PropTypes.func
+}
+
+function ChildA(props, context) {
+    return <div>
+        <h1>ChildA</h1>
+        <h2>a:{context.a}，b:{context.b}</h2>
+        <ChildB />
+    </div>
+}
+
+ChildA.contextTypes = types;
+
+class ChildB extends React.Component {
+
+    /**
+     * 声明需要使用哪些上下文中的数据
+     */
+    static contextTypes = types
+
+    render() {
+        return <p>
+            ChildB，来自于上下文的数据：a: {this.context.a}, b:{this.context.b}
+            <button onClick={() => {
+                this.context.onChangeA(this.context.a + 2);
+            }}>子组件的按钮，a+2</button>
+        </p>
+    }
+}
+
 export default class OldContext extends Component {
 
     /**
-     * 约束上下文中的数据是什么类型
+     * 约束上下文中数据的类型
      */
-    static childContextTypes = {
-        a: PropTypes.number,
-        b: PropTypes.string
-    }
-
-    componentDidMount() {
-        console.log("componentDidMount")
-    }
-
-    componentWillMount() {
-        console.log("componentWillMount")
-    }
-    
+    static childContextTypes = types
 
     state = {
         a: 123,
         b: "abc"
     }
+
     /**
-     * 获取上下文中的数据
+     * 得到上下文中的数据
      */
     getChildContext() {
-        console.log("获取上下文中的数据");
-        return this.state;
+        console.log("获取新的上下文");
+        return {
+            a: this.state.a,
+            b: this.state.b,
+            onChangeA: (newA) => {
+                this.setState({
+                    a: newA
+                })
+            }
+        }
     }
 
     render() {
-        console.log("render");
         return (
             <div>
-                <button onClick={()=>{
-                    this.setState({});
-                }}>改变</button>
+                <ChildA />
+                <button onClick={() => {
+                    this.setState({
+                        a: this.state.a + 1
+                    })
+                }
+                }>a加1</button>
             </div>
         )
     }
