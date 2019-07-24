050d7e456cf6eedd1763f8788eab156868a02ce4
yuanjin <yuanjinmoke@gmail.com>
2019-07-24 16:12:19 +0800

    fixed

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,45 +1,5 @@
 import React from 'react'
-import ErrorBound from "./components/common/ErrorBound"
-
-function Comp1() {
-    return <div style={{
-        width: "90%",
-        height: 500,
-        border: "2px solid"
-    }}>
-        <h1>Comp1</h1>
-        <Comp2 />
-    </div>
-}
-
-
-function Comp2() {
-    return <div style={{
-        width: "70%",
-        height: "70%",
-        border: "2px solid"
-    }}>
-        <h1 onClick={()=>{
-            throw new Error("点击时发生的错误")
-        }}>Comp2</h1>
-    </div>
-}
-
-function Comp3() {
-    return <div style={{
-        width: "90%",
-        height: 500,
-        border: "2px solid"
-    }}>
-        <h1>Comp3</h1>
-    </div>
-}
-
 export default function App() {
     return <div>
-        <ErrorBound>
-            <Comp1 />
-        </ErrorBound>
-        <Comp3 />
     </div>
 }
