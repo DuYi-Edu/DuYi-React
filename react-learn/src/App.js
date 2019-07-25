8219820178ef8ce5d1ebe3d2e914d8bad0307e3d
yuanjin <yuanjinmoke@gmail.com>
2019-07-25 15:43:00 +0800

    phase1

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,5 +1,53 @@
 import React from 'react'
-export default function App() {
-    return <div>
-    </div>
+
+class Comp1 extends React.Component {
+    state = {}
+    constructor(props) {
+        super(props);
+        console.log(4, "Comp1 Constructor")
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log(5, "Comp1 getDerivedStateFromProps")
+        return null;
+    }
+
+    componentDidMount() {
+        console.log("b", "App getDerivedStateFromProps")
+    }
+
+    render() {
+        console.log(6, "Comp1 render");
+        return (
+            <h1>Comp1</h1>
+        )
+    }
+}
+
+export default class App extends React.Component {
+
+    state = {}
+    constructor(props) {
+        super(props);
+        console.log(1, "App Constructor")
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log(2, "App getDerivedStateFromProps")
+        return null;
+    }
+
+    componentDidMount() {
+        console.log("a", "App getDerivedStateFromProps")
+    }
+    
+
+    render() {
+        console.log(3, "App render");
+        return (
+            <div>
+                <Comp1 />
+            </div>
+        )
+    }
 }
--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = <App />;
+ReactDOM.render(app, document.getElementById('root'));
+
+console.log(app);
