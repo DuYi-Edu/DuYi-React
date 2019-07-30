68c8dd8d3e52b16140bc8519f558cb0cca6963bd
yuanjin <yuanjinmoke@gmail.com>
2019-07-30 17:33:18 +0800

    fixed

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import StudentContainer from "./components/StudentContainer"
 
 export default function App() {
     return (
         <div>
-            <StudentContainer />
         </div>
     )
 }
