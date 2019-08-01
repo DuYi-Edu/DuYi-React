de8824d6084556bdbb6a3b64fea71b96e7168e3e
yuanjin <yuanjinmoke@gmail.com>
2019-08-01 14:59:42 +0800

    4-7wanjie

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+const ctx = React.createContext();
+
+// function Test() {
+//     return <ctx.Consumer>
+//         {value => <h1>Test，上下文的值：{value}</h1>}
+//     </ctx.Consumer>
+// }
+
+function Test() {
+    const value = useContext(ctx);
+    return <h1>Test，上下文的值：{value}</h1>
+}
 
 export default function App() {
     return (
         <div>
+            <ctx.Provider value="abc">
+                <Test />
+            </ctx.Provider>
         </div>
     )
 }
