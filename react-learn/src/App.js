5b54edbc97f90147fdc1c1fd5587faf8f306d9a0
yuanjin <yuanjinmoke@gmail.com>
2019-08-12 16:17:32 +0800

    ok

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Transition } from 'react-transition-group';
+import "./App.css"
+const duration = 2000;
 
 export default function App() {
+    const [inProp, setInProp] = useState(true);
     return (
         <div>
+            <Transition in={inProp} timeout={duration}
+                addEndListener={(node, done) => {
+                    node.addEventListener("transitionend", () => {
+                        console.log("过渡结束了")
+                    }, { once: true })
+                }}
+            >
+                {state => {
+                    console.log(state);
+                    return <div className={state}>
+                        I'm a fade Transition!
+                    </div>
+                }}
+
+            </Transition>
+            <button onClick={() => setInProp(!inProp)}>
+                Click to Toggle
+            </button>
         </div>
-    )
+    );
 }
