1a359594cc22a8503c439a788e83f528faee352b
yuanjin <yuanjinmoke@gmail.com>
2019-08-13 10:55:14 +0800

    OK

--- react-learn/src/App copy.js
+++ react-learn/src/App copy.js
@@ -1,20 +1,14 @@
 import React, { useState } from 'react'
 import { SwitchTransition, CSSTransition } from "react-transition-group"
 import "./App.css";
-import "animate.css"
 
 export default function App() {
     const [show1, setShow1] = useState(true)
     return (
-        <div className="container">
+        <div>
             <SwitchTransition mode="out-in">
-                <CSSTransition appear timeout={800} key={show1}
-                    classNames={{
-                        exit: "bounceOut",
-                        enter: "bounceIn"
-                    }}
-                >
-                    <h1 className="animated fast">{show1 ? "title1" : "title2"}</h1>
+                <CSSTransition appear timeout={5000} key={show1}>
+                    <h1>{show1 ? "title1" : "title2"}</h1>
                 </CSSTransition>
             </SwitchTransition>
             <button onClick={() => setShow1(!show1)}>切换</button>
