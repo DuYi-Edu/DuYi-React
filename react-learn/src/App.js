1f8bba3abc6205621b541bee39524344a6b77761
yuanjin <yuanjinmoke@gmail.com>
2019-08-13 11:41:15 +0800

    ok

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import uuid from "uuid";
+import { TransitionGroup, CSSTransition } from "react-transition-group"
+import "./App.css"
 
 export default function App() {
+    const [tasksList, setTasksList] = useState([
+        { id: uuid(), name: "任务1" },
+        { id: uuid(), name: "任务2" },
+    ]);
     return (
         <div>
+            <TransitionGroup appear component="ul" className="abc">
+                {
+                    tasksList.map(t => (
+                        <CSSTransition  timeout={2000} key={t.id}>
+                            <li>{t.name} <button
+                                onClick={() => {
+                                    var newTasks = tasksList.filter(it => it.id !== t.id);
+                                    setTasksList(newTasks);
+                                }}
+                            >删除</button></li>
+                        </CSSTransition>
+                    ))
+                }
+            </TransitionGroup>
+            <button onClick={() => {
+                var name = window.prompt("请输入任务名称");
+                setTasksList([...tasksList, { id: uuid(), name }])
+            }}>添加一个任务</button>
         </div>
+
     )
 }
