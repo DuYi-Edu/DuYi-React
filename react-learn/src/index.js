5fc65a9d4f5a7eef3f8adcdb25c1a2cb24452087
yuanjin <yuanjinmoke@gmail.com>
2019-08-15 15:10:55 +0800

    ok

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App"
-import "./react-router/browserHistory"
-// import "./react-router/hashHistory"
-// import "./react-router/memoryHistory"
-import "./react-router/history"
 
 ReactDOM.render(<App />, document.getElementById('root'));
