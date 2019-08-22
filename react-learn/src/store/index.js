dfe1c96bbe1463185a517c16986d896052957505
yuanjin <yuanjinmoke@gmail.com>
2019-08-22 15:39:15 +0800

    ok

--- react-learn/src/store/index.js
+++ react-learn/src/store/index.js
@@ -1,45 +1,10 @@
 import { createStore, bindActionCreators, applyMiddleware } from "../redux";
 import reducer from "./reducer"
 import { createAddUserAction, createDeleteUserAction } from "./action/usersAction"
+import logger from "redux-logger"
 
 
-/**
- * 一个中间件函数
- * @param {*} store 
- */
-function logger1(store) {
-    return function (next) {
-        //下面返回的函数，是最终要应用的dispatch
-        return function (action) {
-            console.log("中间件1")
-            console.log("旧数据", store.getState());
-            console.log("action", action);
-            next(action);
-            console.log("新数据", store.getState());
-            console.log("")
-        }
-    }
-}
-
-function logger2(store) {
-    return function (next) {
-        //下面返回的函数，是最终要应用的dispatch
-        return function (action) {
-            console.log("中间件2")
-            console.log("旧数据", store.getState());
-            console.log("action", action);
-            next(action);
-            console.log("新数据", store.getState());
-            console.log("")
-        }
-    }
-}
-
-//应用中间件，方式1：
-const store = createStore(reducer, applyMiddleware(logger1, logger2));
-
-//方式2：
-// const store = applyMiddleware(logger1, logger2)(createStore)(reducer)
+const store = createStore(reducer, applyMiddleware(logger));
 
 const actionCreators = {
     addUser: createAddUserAction,
