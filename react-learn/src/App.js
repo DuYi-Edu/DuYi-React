e556a955227005856706e8668690fccf14bd6e51
yuanjin <yuanjinmoke@gmail.com>
2019-09-06 19:17:18 +0800

    ok

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Counter from "./Counter"
 export default function App() {
     return (
         <div>
+            根组件
+            <Counter />
         </div>
     )
 }
