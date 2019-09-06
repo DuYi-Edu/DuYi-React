435da3a3b0c7dfa9d3c8dfda6de49bf59dbb684e
yuanjin <yuanjinmoke@gmail.com>
2019-09-06 18:44:16 +0800

    ok

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from "./App"
+import dva from "dva";
+import counterModel from "./models/counter"
+import studentsModel from "./models/students"
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+//得到一个dva对象
+const app = dva();
+
+//在启动之前定义模型
+app.model(counterModel)
+app.model(studentsModel)
+
+//设置根路由，即启动后，要运行的函数，函数的返回结果会被渲染
+app.router(() => <App />)
+
+app.start("#root")
