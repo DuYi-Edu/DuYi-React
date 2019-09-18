0411af959aae6b8b26a41ff8e926c8aed340f9f4
yuanjin <yuanjinmoke@gmail.com>
2019-09-18 16:29:01 +0800

    ok

---
+++ umi-learn2/.umirc.js
@@ -0,0 +1,22 @@
+
+// ref: https://umijs.org/config/
+export default {
+    treeShaking: true,
+    plugins: [
+        // ref: https://umijs.org/plugin/umi-plugin-react.html
+        ['umi-plugin-react', {
+            dva: true,
+            title: 'umi-learn2',
+
+            routes: {
+                exclude: [
+                    /models\//,
+                    /services\//,
+                    /model\.(t|j)sx?$/,
+                    /service\.(t|j)sx?$/,
+                    /components\//,
+                ],
+            },
+        }],
+    ],
+}
---
+++ umi-learn2/src/pages/.umi/router.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/lib/renderRoutes';
+import history from '@tmp/history';
+import { routerRedux } from 'dva';
+
+const Router = routerRedux.ConnectedRouter;
+
+const routes = [
+  {
+    path: '/',
+    component: require('../../layouts/index.js').default,
+    routes: [
+      {
+        path: '/',
+        exact: true,
+        component: require('../index.js').default,
+        _title: 'umi-learn2',
+        _title_default: 'umi-learn2',
+      },
+      {
+        component: () =>
+          React.createElement(
+            require('D:/repository/React/源码/umi-learn2/node_modules/umi-build-dev/lib/plugins/404/NotFound.js')
+              .default,
+            { pagesPath: 'src/pages', hasRoutesInConfig: false },
+          ),
+        _title: 'umi-learn2',
+        _title_default: 'umi-learn2',
+      },
+    ],
+    _title: 'umi-learn2',
+    _title_default: 'umi-learn2',
+  },
+  {
+    component: () =>
+      React.createElement(
+        require('D:/repository/React/源码/umi-learn2/node_modules/umi-build-dev/lib/plugins/404/NotFound.js')
+          .default,
+        { pagesPath: 'src/pages', hasRoutesInConfig: false },
+      ),
+    _title: 'umi-learn2',
+    _title_default: 'umi-learn2',
+  },
+];
+window.g_routes = routes;
+const plugins = require('umi/_runtimePlugin');
+plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+export { routes };
+
+export default class RouterWrapper extends React.Component {
+  unListen = () => {};
+
+  constructor(props) {
+    super(props);
+
+    // route change handler
+    function routeChangeHandler(location, action) {
+      plugins.applyForEach('onRouteChange', {
+        initialValue: {
+          routes,
+          location,
+          action,
+        },
+      });
+    }
+    this.unListen = history.listen(routeChangeHandler);
+    routeChangeHandler(history.location);
+  }
+
+  componentWillUnmount() {
+    this.unListen();
+  }
+
+  render() {
+    const props = this.props || {};
+    return <Router history={history}>{renderRoutes(routes, props)}</Router>;
+  }
+}
